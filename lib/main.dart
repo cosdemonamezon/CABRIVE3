a4de0274ebe3641bc427016ab21b10dadb096a8b
Mr. Niwat Pumkin1 <niwat_p@dptf.com>
2021-04-06 22:32:46 +0700

    ad

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         ),
         initialRoute: '/',
         routes: <String, WidgetBuilder>{
-          '/': (context) => AvartarScreen(),
+          '/': (context) => LoginScreen(),
           '/Avartar': (context) => AvartarScreen(),
         });
   }
